2bad635e1f8df0d024fcfd193a623a0cdd5a564d
ryan-a-davis365 <itsryzahd@gmail.com>
2025-06-02 18:14:11 +0100

    fix: delete env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_xcly7ozQNI6P@ep-calm-credit-a2tkohxx.eu-central-1.aws.neon.tech/flag_deaf_stuck_339076")
-
-os.environ.setdefault("SECRET_KEY", "j1*@en&vljjtkk62dg#*2+1d-^j7ulj#nrkz6$ecz%-k+rr@6y")
