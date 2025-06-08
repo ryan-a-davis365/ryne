c45696c5c4bdcecdd19e7e28ce368d61617a5eb6
ryan-a-davis365 <itsryzahd@gmail.com>
2025-06-08 14:34:43 +0100

    feat: added checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -27,13 +27,13 @@ class Order(models.Model):
         """
         Generate a random, unique order number using UUID.
         """
-        return uuid.uuid4().hex.upper
+        return uuid.uuid4().hex.upper()
 
     def update_total(self):
         """
         Update grand total and order total each time a line item is added or updated.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
